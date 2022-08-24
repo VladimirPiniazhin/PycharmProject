2241618f3ffa0dedbcd72b860c74ddbf3cdd8909
VladimirPiniazhin <111856833+VladimirPiniazhin@users.noreply.github.com>
2022-08-24 20:08:29 +0300

    Delete Task02.py

--- mod01-tasks/Task02.py
+++
@@ -1,7 +0,0 @@
-#Ympyrän pinra-alan laskeminen
-#Kaava Pii*r2
-#Säde - "radius"
-import math
-radius = input ("Anna ympyrän säde (m):")
-area = math.pi*float(radius)*float(radius)
-print(f"{radius} (m) säteisin ympyrän pinta-ala on {area:.3f} neliömetriä")
