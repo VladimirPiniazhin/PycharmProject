aedc29e7265659ee8a448315f6f2471ff58db197
VladimirPiniazhin <111856833+VladimirPiniazhin@users.noreply.github.com>
2022-08-24 19:56:36 +0300

    Add files via upload

--- mod01-tasks/Task03.py
+++ mod01-tasks/Task03.py
@@ -1,8 +1,12 @@
 #Suorakulmion piiri ja pinra-alan laskeminen
 #Kaava (piiri) kanta*korkeus*2
+#Kaava (pinta-ala) kanta*korkeus/2
 
 suorakulmion_kanta_str = input("Suorakulmion kannan pituus (m):")
 kanta = float(suorakulmion_kanta_str)
 suorakulmion_korkeus_str = input("Suorakulmion korkeus (m):")
 korkeus = float(suorakulmion_korkeus_str)
-print(f"Suorakulmion kannan piiri on {kanta*korkeus*2} (m)")
+piiri = (kanta+korkeus)*2
+pintaala = kanta*korkeus
+print(f"Suorakulmion kannan piiri: {piiri} (m)")
+print(f"Suorakulmion pinta-ala: {pintaala} (m^2)")
