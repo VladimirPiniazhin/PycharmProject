8f02ead8fb986a99b1171e7590e12e166d1189f0
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2022-08-30 10:18:55 +0300

    Initial commit

---
+++ mod02-tasks/valintarakenneKokeiluja.py
@@ -0,0 +1,36 @@
+#rahat = float(input ("Paljonko sulla on rahaa?:"))
+#if rahat >= 5:
+    #print ("Voit ostaa latten")
+#print ("Kiitos kun tstasit riittikö raha, moi!")
+
+#_______________________________________________________
+
+#pituus = float(input ("Kuinka pitkä sinä olet?:"))
+
+#if 1.5 <= pituus <= 2.0:
+    #print ("Olet normaalimittainen")
+
+#if pituus >= 1.5 and pituus <= 2.0:
+    #print("Olet normaalimittainen")
+
+#if pituus <= 1.5 or pituus >= 2.0:
+    #print("Et ole normaalimittainen")
+
+#_______________________________________________________
+
+mjono1 = input("Anna eläinlaji:")
+mjono2 = input("Anna eläinlaji:")
+
+if mjono1 == mjono1:
+    print("Annoit saman lajin!")
+
+
+
+
+
+
+
+
+
+
+
