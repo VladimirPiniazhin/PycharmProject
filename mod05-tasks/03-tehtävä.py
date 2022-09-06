240184f374b2fc4930f56a2b38dfe5b3238543cf
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2022-09-06 17:02:36 +0300

    Initial commit

--- mod05-tasks/03-tehtävä.py
+++ mod05-tasks/03-tehtävä.py
@@ -8,21 +8,26 @@
        # print("Se ei ole alkuluku")
 
 #print("Se ei ole numero")
-d = []
-n = " "
-while n != "":
-    n = input("Syötä numero: ")
-    if n.isnumeric():
-        d.append(int(n))
+#n = " "
+#while n != 1 and n:
+n = int(input("Syötä numero: "))
+for i in range(1, n+1):
+    if 1 <= n <= 3:
+        print("Se on alkuluku")
+        break
+    elif n % i == 0 and n != i:
+        print("Se ei ole alkuluku")
+        break
     else:
-        print("Se ei ole numero")
+        print("Se on alkuluku")
+        break
 
 #d1 = len(d)
 
 #print(len(d))
 
-for i in range(1, n):
-    if n / i == int and (i != int(n) and int(n) / i == 1):
-        print(f"Se on alkuluku")
-    else:
-        print("Se ei ole alkuluku")
+#for i in range(1, n):
+    #if n / i == int and (i != int(n) and int(n) / i == 1):
+        #print(f"Se on alkuluku")
+    #else:
+        #print("Se ei ole alkuluku")
