5d15cce351dc49c88ffd0b4a4c3507afc2030342
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2022-09-06 13:10:57 +0300

    Initial commit

--- mod05-tasks/02-tehtävä.py
+++ mod05-tasks/02-tehtävä.py
@@ -7,7 +7,8 @@ while n != "":
     else:
         print("Se ei ole numero")
 
-d.sort(reverse=True)
-for i in range(0,4):
-    print(f"Suurin numero: {print(d[:4])}")
+print (sorted(d,reverse=True))
+#print(d)
+#for i in range(0,4):
+    #print(f"Suurin numero: {print(d[:4])}")
 #print(f"Yhteensä numeroita on {len(d)}; Max-arvo on {max(d)}; Min-arvo on {min(d)};")
