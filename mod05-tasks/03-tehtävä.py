797c967021f85a5a1eaf310036a808235c2c0df1
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2022-09-06 15:18:04 +0300

    Initial commit

---
+++ mod05-tasks/03-tehtävä.py
@@ -0,0 +1,28 @@
+#n = input("Syötä kokonaisluku: ")
+
+#while n.isnumeric():
+    #if int(n) / 1 == int(n) and int(n) / int(n)  == 1:
+        #print("Se on alkuluku")
+        #break
+    #else:
+       # print("Se ei ole alkuluku")
+
+#print("Se ei ole numero")
+d = []
+n = " "
+while n != "":
+    n = input("Syötä numero: ")
+    if n.isnumeric():
+        d.append(int(n))
+    else:
+        print("Se ei ole numero")
+
+#d1 = len(d)
+
+#print(len(d))
+
+for i in range(1, n):
+    if n / i == int and (i != int(n) and int(n) / i == 1):
+        print(f"Se on alkuluku")
+    else:
+        print("Se ei ole alkuluku")
