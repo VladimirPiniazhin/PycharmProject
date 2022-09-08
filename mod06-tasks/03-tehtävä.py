52c09957908f89ab48345de164aacb93a606cf8e
VladimirPin <vladimir.piniazhin@metropolia.fi>
2022-09-08 20:04:24 +0300

    Initial commit

---
+++ mod06-tasks/03-tehtävä.py
@@ -0,0 +1,10 @@
+def muunnos():
+    gallona = int(input("Anna gallonamäärä: "))
+    while gallona > 0:
+        litra = gallona * 3.785
+        print(f"Se on {litra:.2f} litraa")
+        gallona = int(input("Anna gallonamäärä: "))
+
+    return print("Ohjelma on lopetettu")
+muunnos()
+
