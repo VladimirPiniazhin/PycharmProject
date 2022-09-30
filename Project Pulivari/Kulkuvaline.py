429db9251eeb2e53a641af2f8f75149cd603b5f5
VladimirPin <vladimir.piniazhin@metropolia.fi>
2022-09-30 23:49:06 +0300

    Initial commit

--- Project Pulivari/Kulkuvaline.py
+++ Project Pulivari/Kulkuvaline.py
@@ -1,17 +1,19 @@
 
 
-#def aircraft():
-liitokone = {'name': 'Liitokone', 'speed': 60, 'consumption': 50}
-lentokone = {'name': 'Lentokone', 'speed': 600, 'consumption': 120}
-helikopteri = {'name': 'Helikopteri', 'speed': 300, 'consumption': 100}
-havittaja = {'name': 'Havittaja', 'speed': 1200, 'consumption': 300}
-kuumailmapallo = {'name': 'Kuumailmapallo', 'speed': 50, 'consumption': 10}
+def aircraft_valiko(n):
+    liitokone = {'name': 'Liitokone', 'speed': 60, 'consumption': 50}
+    lentokone = {'name': 'Lentokone', 'speed': 600, 'consumption': 120}
+    helikopteri = {'name': 'Helikopteri', 'speed': 300, 'consumption': 100}
+    havittaja = {'name': 'Havittaja', 'speed': 1200, 'consumption': 300}
+    kuumailmapallo = {'name': 'Kuumailmapallo', 'speed': 50, 'consumption': 10}
+    aircraft_list = (liitokone, lentokone, helikopteri, havittaja, kuumailmapallo)
+    #aircraft_list = k()
+    k = aircraft_list[n]
+    return k
 
-aircraft = (liitokone, lentokone, helikopteri, havittaja, kuumailmapallo)
-    #return aircraft
-
-k = aircraft[1]
+aircraft = aircraft_valiko(0)
+#aircraft_list = aircraft()
 #for key,value in lentokone.items():
     #print(key, ':', value)
-print(k)
-print(f"Valitsit: {k['name']} \nVauhti: {k['speed']} (km/h) \nKulutus: {k['consumption']}")
+#k = aircraft_list[1]
+print(f"Valitsit: {aircraft['name']} \nVauhti: {aircraft['speed']} (km/h) \nKulutus: {aircraft['consumption']}")
