b159fc371f8aec3e97d942a265e51a8495a1ee93
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2022-09-30 10:20:04 +0300

    Initial commit

---
+++ Project Pulivari/Kulkuvaline.py
@@ -0,0 +1,7 @@
+Liitokone = {speed:60, consumption:,airport:}
+Lentokone = {speed:600, consumption:,airport:}
+Helikopteri = {speed:300, consumption:,airport:}
+Havittaja = {speed:1200, consumption:,airport:}
+Kuumailmapallo = {speed:50, consumption:,airport:}
+
+aircraft = (Liitokone,Lentokone,Helikopteri,Havittaja,Kuumailmapallo)
--- Project Pulivari/Main menu.py
+++ Project Pulivari/Main menu.py
@@ -1,3 +1,10 @@
-
+import
 def main_menu():
-    print(f"
+    print(f"Sinä olet Helsingissa.\nValitse kulkuväline")
+    print("1 - Liitokone \n2 - Lentokone \n3 - Helikopteri \n4 - Hävittäjä \n5 - Kuumailmapallo")
+    k = input("Syötä kulkuvälineen numero: ")
+
+    return kulku_valine
+
+
+main_menu()
