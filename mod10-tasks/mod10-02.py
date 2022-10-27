4ca8f38e103b46543396d4ef7515b5c88531c418
VladimirPin <vladimir.piniazhin@metropolia.fi>
2022-10-27 23:51:23 +0300

    Initial commit

--- mod10-tasks/mod10-02.py
+++ mod10-tasks/mod10-02.py
@@ -2,6 +2,30 @@
 # ja ylimmän kerroksen numero sekä hissien lukumäärä. Talon luonnin yhteydessä talo luo tarvittavan määrän hissejä.
 # Hissien lista tallennetaan talon ominaisuutena. Kirjoita taloon metodi aja_hissiä, joka saa parametreinaan hissin
 # numeron ja kohdekerroksen. Kirjoita pääohjelmaan lauseet talon luomiseksi ja talon hisseillä ajelemiseksi.
+class Hissi:
+    "Tämä luokka kuvailee hissin ominaisuksia"
+
+    kerros = 0  #Nykyinen kerros
+    def __init__(self, alimman, ylimman):
+        self.alimman = alimman
+        self.ylimman  = ylimman
+
+    def siirry_kerrokseen(self, n):
+        while self.kerros != n:
+           if n > self.kerros:
+               self.kerros_ylos()
+               print(f"Nyt on {self.kerros}")
+           elif n < self.kerros:
+               self.kerros_alas()
+               print(f"Nyt on {self.kerros}")
+        print(f"Saapuneet {self.kerros}")
+    def kerros_ylos (self):
+       self.kerros = self.kerros + 1
+
+    def kerros_alas(self):
+       self.kerros = self.kerros - 1
+
+
 class Talo:
     "Tämä luokka kuvailee hissin ominaisuksia"
 
@@ -12,26 +36,13 @@ class Talo:
         self.ylimman  = ylimman
         self.h_maara = h_maara
 
-    def hissijen_maara(self):
         for i in range(self.h_maara):
-            self.hissit.append(Hissi(self))
-    class Hissi:
+            self.hissit.append(Hissi)
+    def aja_hissia(self, num, n):
+        #for self.hissit[num] in self.hissit:
 
-                def __init__(self, alimman, ylimman):
-            self.alimman = alimman
-            self.ylimman  = ylimman
+# PÄÄOHJELMA
 
-        def siirry_kerrokseen(self, n):
-            while self.kerros != n:
-               if n > self.kerros:
-                   self.kerros_ylos()
-                   print(f"Nyt on {self.kerros}")
-               elif n < self.kerros:
-                   self.kerros_alas()
-                   print(f"Nyt on {self.kerros}")
-            print(f"Saapuneet {self.kerros}")
-        def kerros_ylos (self):
-           self.kerros = self.kerros + 1
+t = Talo(0,10,3)
 
-        def kerros_alas(self):
-           self.kerros = self.kerros - 1
+print(t.hissit[1].kerros)
