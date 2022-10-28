712d7a91a1060f6b2584ca83c0365c4e8a77723e
Vladimirpiniazhin <vladimir.piniazhin@metropolia.fi>
2022-10-28 16:08:36 +0300

    Initial commit

--- mod10-tasks/mod10-02.py
+++ mod10-tasks/mod10-02.py
@@ -11,14 +11,17 @@ class Hissi:
         self.ylimman  = ylimman
 
     def siirry_kerrokseen(self, n):
+        if n < self.alimman or n > self.ylimman:
+            return
         while self.kerros != n:
            if n > self.kerros:
                self.kerros_ylos()
-               print(f"Nyt on {self.kerros}")
+               print(f"Nyt on {self.kerros} kerros")
            elif n < self.kerros:
                self.kerros_alas()
-               print(f"Nyt on {self.kerros}")
-        print(f"Saapuneet {self.kerros}")
+               print(f"Nyt on {self.kerros} kerros")
+        print(f"Olemme saapuneet {self.kerros}")
+
     def kerros_ylos (self):
        self.kerros = self.kerros + 1
 
@@ -27,28 +30,16 @@ class Hissi:
 
 
 class Talo:
-    "Tämä luokka kuvailee hissin ominaisuksia"
+    "Tämä luokka kuvailee talon ominaisuksia"
 
-    kerros = 0  #Nykyinen kerros
-    hissit = []
     def __init__(self, alimman, ylimman, h_maara):
-        self.alimman = alimman
-        self.ylimman  = ylimman
         self.h_maara = h_maara
-
+        self.hissit = []
         for i in range(self.h_maara):
-            self.hissit.append(Hissi)
+            self.hissit.append(Hissi(alimman, ylimman))
+
     def aja_hissia(self, num, n):
-        while self.hissit[num].kerros != n:
-            if n > self.hissit[num].kerros:
-                #self.hissit[num].kerros_ylos(self)
-                self.hissit[num].kerros += 1
-                print(f"Nyt on {self.hissit[num].kerros}")
-            elif n < self.hissit[num].kerros:
-                #self.hissit[num].kerros_alas(self)
-                self.hissit[num].kerros -= 1
-                print(f"Nyt on {self.hissit[num].kerros}")
-        print(f"Saapuneet {self.hissit[num].kerros}")
+        self.hissit[num].siirry_kerrokseen(n)
 
 # PÄÄOHJELMA
 
@@ -56,6 +47,4 @@ t = Talo(0,10,3)
 #print(t.h_maara)
 print(t.hissit[1].kerros)
 t.aja_hissia(1, 3)
-print(t.hissit[1].kerros)
-t.aja_hissia(1, 6)
-print(t.hissit[1].kerros)
+print(f"Olet {t.hissit[1].kerros}. kerroksessa")
