1068a80b36d43be62c393e430be7163484761b06
VladimirPin <vladimir.piniazhin@metropolia.fi>
2022-10-30 16:44:01 +0200

    Initial commit

--- mod12-tasks/mod12-01.py
+++ mod12-tasks/mod12-01.py
@@ -9,4 +9,13 @@ vastaus = requests.get(pyynto).json()
 #print(vastaus)
 #print(json.dumps(vastaus, indent=2))
 
-print(vastaus["value"])
+#print(vastaus["value"])
+try:
+    vastaus = requests.get(pyynto)
+    if vastaus.status_code==200:
+        json_vastaus = vastaus.json()
+        print(json.dumps(json_vastaus, indent=2))
+        #for a in json_vastaus:
+        print(json_vastaus["value"])
+except requests.exceptions.RequestException as e:
+    print("Hakua ei voitu suorittaa.")
